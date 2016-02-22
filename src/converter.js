1218f97a1f8192df4617c9ad998c1e505b3acbbc
Yumin <lym91@hatiolab.com>
2016-02-22 16:16:21 +0900

    바코드 확인 및 수정

--- src/converter.js
+++ src/converter.js
@@ -245,7 +245,7 @@ var commandMap = new Map([
 		return obj;
 	}],
 	['BM', function(params) {
-		var obj = oehfge2(params);
+		var obj = oehfge(params);
 		obj.symbol = 'msi'
 		return obj;
 	}],
@@ -405,6 +405,7 @@ function ohfge(params) {
 		obj.height = p[1];
 		obj.showText = p[2];
 		obj.textAbove = p[3];
+		obj.checkDigit = p[4];
 
 		return obj;
 }
@@ -431,11 +432,27 @@ function ohfm(params) {	// ^B4o,h,f,m : m: starting mode
 			obj.textAbove = 'N';
 	}
 
+	obj.mode = p[3];
+
 	return obj;
 }
 
 // 바코드 생성 커맨드의 파라미터가 o,h,f,g 일때 호출
 function ohfge(params) {
+		var p = params.split(',');
+		
+		var obj = {};
+		obj.type = 'barcode';
+		obj.rot = p[0];
+		obj.height = p[1];
+		obj.showText = p[2];
+		obj.textAbove = p[3];
+		obj.checkDigit = p[4];
+
+		return obj;
+}
+
+function ohfg(params) {
 		var p = params.split(',');
 		
 		var obj = {};
@@ -473,6 +490,7 @@ function ohscrm(params) {
 		obj.security = p[2];
 		obj.columns = p[3];
 		obj.rows = p[4];
+		obj.mode = p[5];
 
 		return obj;
 }
@@ -557,6 +575,18 @@ function ohscrfg(params) {
 		obj.rows = p[4];
 		obj.format = p[5];
 		obj.escape = p[6];
+
+		return obj;
+}
+
+function mpt(params) {
+		var p = params.split(',');
+		
+		var obj = {};
+		obj.type = 'barcode';
+		obj.mode = p[0];
+		obj.position = p[1];
+		obj.total = p[2];
 
 		return obj;
 }
