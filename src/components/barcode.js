ed63867fdee63516b2fea591317ecef92e4e8679
Lee-YuMin <lym91@hatiolab.com>
2016-03-11 15:42:53 +0900

    ean13 - symbol 수정

--- src/components/barcode.js
+++ src/components/barcode.js
@@ -15,7 +15,6 @@ function barcode(properties) {
 		var scale_w = this.model.scale_w || '';
 		var scale_h = this.model.scale_h || '';
 
-
 		var scale = '';
 		var lines = [];
 		if(scaleBuf.w != scale_w || scaleBuf.h != scale_h) {
@@ -28,28 +27,27 @@ function barcode(properties) {
 			scale_w = '';
 		}
 
-
 		var symbolMap = new Map([
 			['code11', 				['^B1'+rotate, , height, showText, textAbove]],
 			['interleaved2of5', 	['^B2'+rotate, height, showText, textAbove, ]],
 			['code39', 				['^B3'+rotate, , height, showText, textAbove]],
 			['code49', 				['^B4'+rotate, height, showText,]],
 			['planet', 				['^B5'+rotate, height, showText, textAbove]],
-			['pdf417', 				['^B7'+rotate, height, , , , ]],
+			['pdf417', 			x	['^B7'+rotate, height, , , , ]],
 			['ean8', 				['^B8'+rotate, height, showText, textAbove]],
 			['upce', 				['^B9'+rotate, height, showText, textAbove, ]],
 			['code93', 				['^BA'+rotate, height, showText, textAbove, ]],
 			['codablock', 			['^BB'+rotate, height, , , , ]],
 			['code128', 			['^BC'+rotate, height, showText, textAbove, , ]],
 			['maxicode', 			['^BD'+rotate, , height, showText, textAbove]],
-			['ean13', 				['^BE'+rotate, , height, showText, textAbove]],
+			['ean13', 				['^BE'+rotate, height, showText, textAbove]],
 			['micropdf417', 		['^BF'+'2', , ]],
 			['industrial2of5',		['^BI'+rotate, height, showText, textAbove]],
 			['standard2of5', 		['^BJ'+rotate, height, showText, textAbove]],
 			['ansicodabar', 		['^BK'+rotate, , height, showText, textAbove, , ]],
 			['logmars', 			['^BL'+rotate, height, textAbove]],
 			['msi', 				['^BM'+rotate, , height, showText, textAbove, ]],
-			['plessey', 			['^BP'+rotate, ,  height, showText, textAbove]],
+			['plessey', 			['^BP'+rotate, , height, showText, textAbove]],
 			['qrcode', 				['^BQ'+'']],	// TODO
 			['upca', 				['^BU'+rotate, height, showText, textAbove, ]],
 			['datamatrix', 			['^BX'+'']],	// TODO
@@ -58,7 +56,6 @@ function barcode(properties) {
 
 		
 		var params = symbolMap.get(symbol);
-
 
 		
 		lines.push('^FO' + left + ',' + top)
