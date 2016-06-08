97d1f70eb21b18f69b91eb2f1011c1b8cf0b5032
SunChunxie <c.s@SunChunxies-MacBook-Pro.local>
2016-06-08 21:36:35 +0900

    build

--- things-zpl.js
+++ things-zpl.js
@@ -1,7 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var config = {
 	fontNo: 6,
-	dpi: 200
+	dpi: 203
+	// dpi: 300
 }
 
 exports.config = config;
@@ -1744,7 +1745,7 @@ function calcDotSize(model) {
 
     var size = model[property];
     if (typeof size === 'number') {
-      model[property] = config.dpi * size / 25.4;
+      model[property] = Math.round(config.dpi * size / 25.4);
     }
   }
 }
@@ -1963,8 +1964,6 @@ function makeZpl(components, zpl) {
 	}
 
 	components.forEach(function (c) {
-		calcDotSize(c);
-
 		switch (c.type) {
 			case 'group':
 				groups.push(new Group(c));
